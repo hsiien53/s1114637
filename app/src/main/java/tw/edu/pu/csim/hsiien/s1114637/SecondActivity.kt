f01cbb5393160043c69f5de3a15fab8018bfb4aa
hsiien53 <hsien03593@gmail.com>
2024-05-22 16:21:08 +0800

    v4_蔡譯嫺

--- app/src/main/java/tw/edu/pu/csim/hsiien/s1114637/SecondActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/hsiien/s1114637/SecondActivity.kt
@@ -1,10 +1,14 @@
 package tw.edu.pu.csim.hsiien.s1114637
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -26,12 +30,17 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import tw.edu.pu.csim.hsiien.s1114637.ui.theme.S1114637Theme
 
@@ -50,6 +59,79 @@ class SecondActivity : ComponentActivity() {
                 }
             }
         }
+    }
+}
+
+@Composable
+fun FirstScreen1(navController: NavController) {
+    val context = LocalContext.current
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.Start
+    ) {
+        Text(
+            text = "「台中市愛心家園」經市政府公開評選後，委託瑪利亞基金會經營管理，於91年啟用，整棟建築物有四個樓層，目前開辦就醫、就養、就學、就業四大領域的十項業務，提供身心障礙者全方位的服務。\n",
+            color = Color.Black
+        )
+        Text(
+            text = "長按以下圖片，可以觀看愛心家園地圖",
+            color = Color.Blue
+        )
+        Image(
+            painter = painterResource(id = R.drawable.lovehome),
+            contentDescription = "button icon",
+            modifier = Modifier
+                .pointerInput(Unit) {
+                    detectTapGestures(
+                        onLongPress = {
+                            val intent = Intent(Intent.ACTION_VIEW)
+                            intent.data = Uri.parse("geo:0,0?q=台中市南屯區東興路一段450號")
+                            context.startActivity(intent)
+                        }
+                    )
+                }
+        )
+    }
+}
+
+@Composable
+fun SecondScreen2(navController: NavController) {
+    val context = LocalContext.current
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.Start
+    ) {
+        Text(
+            text = "「瑪利亞學園」提供重度以及極重度多重障礙者日間照顧服務，以健康照護為基礎，支持生活多面向參與及學習概念，輔助發展重度身心障礙者自我概念為最終服務目標。\n",
+            color = Color.Black
+        )
+        Text(
+            text = "雙擊以下圖片，可以觀看瑪利亞學園地圖",
+            color = Color.Blue
+        )
+        Image(
+            painter = painterResource(id = R.drawable.campus),
+            contentDescription = "button icon",
+            modifier = Modifier
+
+                .pointerInput(Unit) {
+                    detectTapGestures(
+                        onDoubleTap = {
+                            val intent = Intent(Intent.ACTION_VIEW)
+                            intent.data = Uri.parse("geo:0,0?q=台中市北屯區經貿東路365號")
+                            context.startActivity(intent)
+                        }
+                    )
+                }
+        )
     }
 }
 
@@ -100,17 +182,25 @@ fun Main3() {
         Text(
             text = "主要機構", color = Color.Red
         )
-
-
-
-
-
-
-
-
-
-
-
-
+        Row {
+            Button(onClick = {
+                navController.navigate("JumpFirst1")
+            }) {
+                Text(text = "台中市愛心家園")
+            }
+            Button(onClick = {
+                navController.navigate("JumpSecond2")
+            }) {
+                Text(text = "瑪利亞學園")
+            }
+        }
+        NavHost(navController = navController, startDestination = "JumpFirst1") {
+            composable("JumpFirst1") {
+                FirstScreen1(navController = navController)
+            }
+            composable("JumpSecond2") {
+                SecondScreen2(navController = navController)
+            }
+        }
     }
 }
