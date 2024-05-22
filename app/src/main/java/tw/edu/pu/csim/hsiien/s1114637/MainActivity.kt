4dcc5e2a18c141f96ab4cd8ec97b52ca76bd5969
hsiien53 <hsien03593@gmail.com>
2024-05-22 15:42:07 +0800

    v0

---
+++ app/src/main/java/tw/edu/pu/csim/hsiien/s1114637/MainActivity.kt
@@ -0,0 +1,46 @@
+package tw.edu.pu.csim.hsiien.s1114637
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import tw.edu.pu.csim.hsiien.s1114637.ui.theme.S1114637Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            S1114637Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    S1114637Theme {
+        Greeting("Android")
+    }
+}
